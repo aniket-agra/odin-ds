e12f8a646b2fbc39df52acd7c999e1c1bd39954a
Aniket Agrawal <aniket.mpa@gmail.com>
2023-07-09 22:31:25 +0200

    Add breadth traversal of tree

--- src/binaryTree.js
+++ src/binaryTree.js
@@ -46,14 +46,36 @@ function createTree(rootNode = null) {
         return treeRoot.getValue() + "_" + preOrder(treeRoot.getLeft()) + "_" + preOrder(treeRoot.getRight());
     }
 
-        const postOrder = function (treeRoot) {
+    const postOrder = function (treeRoot) {
         if (treeRoot === null) {
             return "";
         }
         return postOrder(treeRoot.getLeft()) + "_" + postOrder(treeRoot.getRight()) + "_" + treeRoot.getValue();
     }   
 
-    return {append, getRoot, inOrder, preOrder, postOrder};
+    const breadthTraverse = function (treeRoot) {
+        if (treeRoot === null) {
+            return "";
+        } else {
+            let result = "";
+            let prevNodes = [treeRoot], currNodes = [];
+            while(prevNodes.length > 0) {
+                for (let i = 0; i < prevNodes.length; i++) {
+                    result = result + prevNodes[i].getValue() + " ";
+                    if (prevNodes[i].getLeft() !== null) 
+                        currNodes.push(prevNodes[i].getLeft());
+                    if (prevNodes[i].getRight() !== null) 
+                        currNodes.push(prevNodes[i].getRight());
+                }
+                prevNodes = currNodes;
+                currNodes = [];
+                result = result + "\n";
+            }
+            return result;
+        }
+    }
+
+    return {append, getRoot, inOrder, preOrder, postOrder, breadthTraverse};
 }
 
 export{createTree};
